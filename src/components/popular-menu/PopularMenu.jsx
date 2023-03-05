81419edf66ed359d0a0f4271b6b9a77de983c527
Rovshan13 <allakhverdievrovshann@gmail.com>
2023-03-05 17:50:32 +0400

    feat: PopularMenu and Slider

---
+++ src/components/popular-menu/PopularMenu.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+import { Container, Row, Col } from "reactstrap";
+import {
+  popularMenuFood,
+} from "../../assets/fake-data/products";
+import ProductCard from "../product-card/ProductCard";
+
+function PopularMenu() {
+  return (
+    <section className='pt-0'>
+      <Container>
+        <Row>
+          <Col lg="12" className="mb-5">
+            <h2>Polular food menu</h2>
+          </Col>
+
+          {popularMenuFood.map((item) => (
+            <Col lg="3" key={item.id}>
+              <ProductCard item={item} />
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    </section>
+  );
+}
+
+export default PopularMenu;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,16 @@
 import React, { Fragment } from 'react'
 
 import Header from '../components/header/Header'
+import HeroSlider from '../components/hero-slider/HeroSlider'
+import PopularMenu from '../components/popular-menu/PopularMenu'
+
 
 function Home() {
   return (
    <Fragment>
         <Header />
+        <HeroSlider />
+        <PopularMenu />
    </Fragment>
   )
 }
